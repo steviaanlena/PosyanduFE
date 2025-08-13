e3b54f0753e9c7ab22bb37fd8d14ac866a6e452d
steviaanlena <stevia.ap@gmail.com>
2025-08-13 13:51:19 +0700

    Add vite config to fix Vercel build issues

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'recharts': ['recharts'],
+          'react-icons': ['react-icons']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: [
+      'recharts',
+      'react-icons/fa'
+    ]
+  },
+  ssr: {
+    noExternal: ['recharts', 'react-icons']
+  }
 })
